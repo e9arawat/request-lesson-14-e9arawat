a89ac5b3e8a1725c7a0e11acbe43b8ab002f8309
e9arawat <153479261+e9arawat@users.noreply.github.com>
2023-12-31 20:26:57 +0530

    Completed

--- answer.py
+++ answer.py
@@ -1,24 +1,35 @@
 import requests
 import re
 import random
-
-def answer(input_url: str = 'https://httpbin.org'):
-    get_object = requests.get(input_url)
-    total_urls = re.findall('["\'](https?://\S+["\'])', get_object.text)
+def answer(n: int=1, url: str='https://httpbin.org'):
+    get_object = requests.get('https://httpbin.org')
+    total_urls = re.findall('["\'](https?://\S+)["\']', get_object.text)
     if not total_urls:
-        return "No links present"
-    no_of_random_urls = random.randint(len(total_urls)//2,len(total_urls))
+        return "No url found"
+    current_urls = total_urls.copy()
+    for _ in range(n):
+        iterator_urls = []
+        for i in current_urls:
+            temp_object = requests.get(i)
+            temp_urls = re.findall('["\'](https?://\S+)["\']', temp_object.text)
+            iterator_urls = iterator_urls + temp_urls
+        total_urls += iterator_urls
+    
+    number_of_random_urls = random.randint(len(total_urls)//2, len(total_urls))
 
-    random_url_list = random.choices(total_urls, k=no_of_random_urls)
+    random_urls = random.choices(total_urls, k=number_of_random_urls)
+
     ans = {}
-    for j in random_url_list:
-        i = j[:-1]
-        current_object = requests.get(i)
-        current_status_code = current_object.status_code
-        if current_status_code in ans:
-            ans[current_status_code].append(i)
+    for i in random_urls:
+        try:
+            r = requests.get(i)
+        except Exception as e:
+            print("Unable to establish new connection")
+        if r.status_code in ans:
+            ans[r.status_code].append(i)
         else:
-            ans[current_status_code] = [i]
+            ans[r.status_code] = [i]
+    
     return ans
 
 if __name__ == "__main__":
